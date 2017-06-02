4f8ee67eedd217aa9c4ca4d3ea5149bc488ebac5
gravesjake <jacob.graves@ucdenver.edu>
2017-06-02 16:52:17 -0600

    Created and populated a database of FCC Grantees

--- update_grantees.py
+++ update_grantees.py
@@ -9,7 +9,6 @@ def fetch_grantees():
     print(dir(r))
     print(r.headers['content-type'])
     print(r.encoding)
-    # print(r.text)
 
 attributes = [
     'grantee_code',
@@ -30,52 +29,45 @@ def parse_grantees():
     tree = ET.parse('grantees.xml')
     root = tree.getroot()
     for row in root.iter('Row'):
-        rows.append(dict())
+        grantee = []
         for attribute in attributes:
             x = row.find(attribute)
             if hasattr(x, 'text'):
-                rows[-1][attribute] = x.text
+                grantee.append(x.text)
+        rows.append(grantee)
     print("Found %d grantees" % len(rows))
+    return rows
 
-def write_db():
-    conn = sqlite3.connect('/tmp/example')
+def write_db(granteeTest):
+    conn = sqlite3.connect('FCCgrantees.db')
     c = conn.cursor()
 
     c.execute("""drop table if exists grantees""")
-
     conn.commit()
 
-    c.execute("""create table towns (
-            tid int primary key not NULL ,
-            name text,
-            postcode text)""")
+    c.execute('''CREATE TABLE grantees
+                (grantee_code int primary key not NULL,  
+                grantee_name text,
+                mailing_address text,
+                po_box text,
+                city text,
+                state text,
+                country text,
+                zip_code text,
+                contact_name text,
+                date_received text)''')
 
-    c.execute("""create table hotels (
-            hid int primary key not NULL ,
-            tid int,
-            name text,
-            address text,
-            rooms int,
-            rate float)""")
-
-    c.execute("""insert into towns values (1, "Melksham", "SN12")""")
-    c.execute("""insert into towns values (2, "Cambridge", "CB1")""")
-    c.execute("""insert into towns values (3, "Foxkilo", "CB22")""")
-
-    c.execute("""insert into hotels values (1, 2, "Hamilkilo Hotel", "Chesterton Road", 15, 40.)""")
-    c.execute("""insert into hotels values (2, 2, "Arun Dell", "Chesterton Road", 60, 70.)""")
-    c.execute("""insert into hotels values (3, 2, "Crown Plaza", "Downing Street", 100, 105.)""")
-    c.execute("""insert into hotels values (4, 1, "Well House Manor", "Spa Road", 5, 80.)""")
-    c.execute("""insert into hotels values (5, 1, "Beechfield House", "The Main Road", 26, 110.)""")
-
+    c.executemany('INSERT INTO grantees VALUES (?,?,?,?,?,?,?,?,?,?)', granteeTest)
     conn.commit()
-
-    c.execute ("""select * from towns left join hotels on towns.tid = hotels.tid""")
 
     for row in c:
-        print (row)
+        print(row)
 
     c.close()
+    print("Table Created")
+
 
 if __name__ == "__main__":
-    parse_grantees()
+    grantees = parse_grantees()
+    write_db(grantees)
+    
